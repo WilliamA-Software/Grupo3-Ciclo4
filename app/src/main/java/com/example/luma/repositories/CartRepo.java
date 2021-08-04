66e464830ec9e0d10c7c3eeb219fa7a4527527a3
fasadif <fasadif@yahoo.com>
2021-08-04 14:20:44 -0500

    Avoid double product view at select same product

--- app/src/main/java/com/example/luma/repositories/CartRepo.java
+++ app/src/main/java/com/example/luma/repositories/CartRepo.java
@@ -28,14 +28,16 @@ public class CartRepo {
             initCart();
         }
         List<CartProduct> cartProductList = new ArrayList<>(mutableCart.getValue());
+        // Recorrer la lista del carrito en busca de productos repetidos
         for (CartProduct cartProduct: cartProductList){
             if (cartProduct.getProduct().getNameProduct().equals(product.getNameProduct())){
                 int index = cartProductList.indexOf(cartProduct);
                 cartProduct.setQuantity(cartProduct.getQuantity() + 1);
                 cartProductList.set(index, cartProduct);
+                mutableCart.setValue(cartProductList);
+                return true;
             }
         }
-
         CartProduct cartProduct = new CartProduct(product, 1);
         cartProductList.add(cartProduct);
         mutableCart.setValue(cartProductList);
